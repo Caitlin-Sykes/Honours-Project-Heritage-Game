ac6938bac34f51eab473d86b586ccbdd2426713a
Cait <2423179@dundee.ac.uk>
2023-11-12 16:33:22 +0000

    updated to one theme

--- scripts/menus/OptionsMenu.cs
+++ scripts/menus/OptionsMenu.cs
@@ -6,6 +6,10 @@ public partial class OptionsMenu : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// var t = Theme.L();
+// t.set_color("font_color", "Label", Color(1.0, 1.0, 1.0))
+		// Theme theme = (Theme)GD.Load("res://resources/themes/main_theme.tres");
+		// Control.the = theme;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
